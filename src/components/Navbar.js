34ddb7e50af5cf0cf2e84cb3d6c574733b3543aa
Andriy <napuwunapuwu@gmail.com>
2020-01-08 22:58:07 +0200

    Added context API and css for the product tile

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,40 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
+import styled from 'styled-components';
+import { ButtonContainer } from './Button';
+import logo from '../logo_shop.svg';
 
 export default class Navbar extends Component {
     render() {
         return (
-            <div>
-                <h3>Navbar + menu</h3>
-            </div>
+            <NavWrapper className="navbar navbar-expand-sm  navbar-dark px-sm-5">                
+                <Link to='/'>
+                    <img src={logo} alt='store' className='navbar-brand' />
+                </Link>                
+                <ul className="navbar-nav align-items-center">
+                    <li className="nav-item ml-5"></li>
+                    <Link to="/" className="nav-link">
+                        products
+                    </Link>
+                </ul>  
+                <Link to='/cart' className="ml-auto">
+                    <ButtonContainer>
+                        <span className="mr-2">
+                            <i className="fas fa-cart-plus" />
+                        </span>                        
+                        my cart
+                    </ButtonContainer>
+                </Link>
+            </NavWrapper>
         )
     }
 }
+
+const NavWrapper = styled.nav`
+background: var(--mainBlue);
+.nav-link{
+    color: var(--mainWhite)!important;
+    font-size: 1.3rem;
+    text-transform: capitalize;
+}
+`
